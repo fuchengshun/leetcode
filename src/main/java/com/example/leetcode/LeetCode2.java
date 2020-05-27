32cadb76cd45d34b1f970ae94065d2ee530a5776
fuchengshun <82375223@qq.com>
2020-05-27 19:53:27 +0800

    89. 格雷编码

--- src/main/java/com/example/leetcode/LeetCode2.java
+++ src/main/java/com/example/leetcode/LeetCode2.java
@@ -1,6 +1,13 @@
 package com.example.leetcode;
 
 
+import org.omg.CORBA.ARG_IN;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Stack;
+
 public class LeetCode2 {
     /**
      * 给你一个字符串 S、一个字符串 T，请在字符串 S 里面找出：包含 T 所有字符的最小子串。
@@ -171,20 +178,220 @@ public class LeetCode2 {
      * @return
      */
     public ListNode reverseKGroup(ListNode head, int k) {
-        ListNode start = reverseKGroupHelper(head, k), cur = start;
-        for (int i = 0; i < k && cur != null; i++) {
+        if (head == null) {
+            return null;
+        }
+        Stack<ListNode> stack = new Stack<>();
+        ListNode headTemp = head;
+        for (int i = 0; i < k; i++) {
+            stack.add(headTemp);
+            if (headTemp == null) {
+                return head;
+            }
+            headTemp = headTemp.next;
+        }
+        ListNode start = new ListNode(0), cur = start;
+        while (!stack.isEmpty()) {
+            cur.next = stack.pop();
             cur = cur.next;
         }
         if (cur != null) {
-            cur.next = reverseKGroup(cur, k);
+            cur.next = reverseKGroup(headTemp, k);
         }
-        return start;
+        return start.next;
     }
 
     /**
-     * 翻转前k个元素
+     * 给定一个整数数组 A，返回其中元素之和可被 K 整除的（连续、非空）子数组的数目。
+     * <p>
+     *  
+     * <p>
+     * 示例：
+     * <p>
+     * 输入：A = [4,5,0,-2,-3,1], K = 5
+     * 输出：7
+     * 解释：
+     * 有 7 个子数组满足其元素之和可被 K = 5 整除：
+     * [4, 5, 0, -2, -3, 1], [5], [5, 0], [5, 0, -2, -3], [0], [0, -2, -3], [-2, -3]
+     *  
+     * <p>
+     * 提示：
+     * <p>
+     * 1 <= A.length <= 30000
+     * -10000 <= A[i] <= 10000
+     * 2 <= K <= 10000
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/subarray-sums-divisible-by-k
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     *
+     * @param A
+     * @param K
+     * @return
      */
-    public ListNode reverseKGroupHelper(ListNode head, int k) {
-        return null;
+    public int subarraysDivByK(int[] A, int K) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        int tempSum = 0, ans = 0;
+        for (int i = 0; i < A.length; i++) {
+            tempSum += A[i];
+            int key = (tempSum % K + K) % K;
+            ans += map.getOrDefault(key, 0);
+            map.put(key, map.getOrDefault(key, 0) + 1);
+        }
+        return ans;
+    }
+
+    /**
+     * 给定一个链表，旋转链表，将链表每个节点向右移动 k 个位置，其中 k 是非负数。
+     * <p>
+     * 示例 1:
+     * <p>
+     * 输入: 1->2->3->4->5->NULL, k = 2
+     * 输出: 4->5->1->2->3->NULL
+     * 解释:
+     * 向右旋转 1 步: 5->1->2->3->4->NULL
+     * 向右旋转 2 步: 4->5->1->2->3->NULL
+     * 示例 2:
+     * <p>
+     * 输入: 0->1->2->NULL, k = 4
+     * 输出: 2->0->1->NULL
+     * 解释:
+     * 向右旋转 1 步: 2->0->1->NULL
+     * 向右旋转 2 步: 1->2->0->NULL
+     * 向右旋转 3 步: 0->1->2->NULL
+     * 向右旋转 4 步: 2->0->1->NULL
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/rotate-list
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     *
+     * @param head
+     * @param k
+     * @return
+     */
+    public ListNode rotateRight(ListNode head, int k) {
+        ArrayList<ListNode> list = new ArrayList<>();
+        while (head != null) {
+            list.add(head);
+            head = head.next;
+        }
+        if (list.size() == 0) {
+            return null;
+        }
+        ListNode start = new ListNode(0), cur = start;
+        k %= list.size();
+        for (int i = 0; i < list.size(); i++) {
+            cur.next = list.get((list.size() - k + i) % list.size());
+            cur = cur.next;
+        }
+        cur.next = null;
+        return start.next;
+    }
+
+    /**
+     * 给定一组不含重复元素的整数数组 nums，返回该数组所有可能的子集（幂集）。
+     * <p>
+     * 说明：解集不能包含重复的子集。
+     * <p>
+     * 示例:
+     * <p>
+     * 输入: nums = [1,2,3]
+     * 输出:
+     * [
+     * [3],
+     *   [1],
+     *   [2],
+     *   [1,2,3],
+     *   [1,3],
+     *   [2,3],
+     *   [1,2],
+     *   []
+     * ]
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/subsets
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     *
+     * @param nums
+     * @return
+     */
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        subsetsDfs(nums, new ArrayList<>(), ans);
+        return ans;
+    }
+
+    public void subsetsDfs(int[] nums, ArrayList<Integer> path, List<List<Integer>> ans) {
+        if (path.size() == nums.length) {
+            ArrayList<Integer> temp = new ArrayList<>();
+            for (Integer num : path) {
+                if (num != null) {
+                    temp.add(num);
+                }
+            }
+            ans.add(temp);
+            return;
+        }
+        path.add(nums[path.size()]);
+        subsetsDfs(nums, path, ans);
+        path.remove(path.size() - 1);
+        path.add(null);
+        subsetsDfs(nums, path, ans);
+        path.remove(path.size() - 1);
+    }
+
+    /**
+     * 格雷编码是一个二进制数字系统，在该系统中，两个连续的数值仅有一个位数的差异。
+     * <p>
+     * 给定一个代表编码总位数的非负整数 n，打印其格雷编码序列。即使有多个不同答案，你也只需要返回其中一种。
+     * <p>
+     * 格雷编码序列必须以 0 开头。
+     * <p>
+     *  
+     * <p>
+     * 示例 1:
+     * <p>
+     * 输入: 2
+     * 输出: [0,1,3,2]
+     * 解释:
+     * 00 - 0
+     * 01 - 1
+     * 11 - 3
+     * 10 - 2
+     * <p>
+     * 对于给定的 n，其格雷编码序列并不唯一。
+     * 例如，[0,2,3,1] 也是一个有效的格雷编码序列。
+     * <p>
+     * 00 - 0
+     * 10 - 2
+     * 11 - 3
+     * 01 - 1
+     * 示例 2:
+     * <p>
+     * 输入: 0
+     * 输出: [0]
+     * 解释: 我们定义格雷编码序列必须以 0 开头。
+     *      给定编码总位数为 n 的格雷编码序列，其长度为 2n。当 n = 0 时，长度为 20 = 1。
+     *      因此，当 n = 0 时，其格雷编码序列为 [0]。
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/gray-code
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     *
+     * @param n
+     * @return
+     */
+    public List<Integer> grayCode(int n) {
+        if (n == 0) {
+            ArrayList<Integer> res = new ArrayList<>();
+            res.add(0);
+            return res;
+        }
+        List<Integer> ans = grayCode(n - 1);
+        for (int i = ans.size() - 1; i >= 0; i--) {
+            ans.add(ans.get(i) + (1 << n - 1));
+        }
+        return ans;
     }
 }
--- src/test/java/com/example/leetcode/LeetcodeApplicationTests.java
+++ src/test/java/com/example/leetcode/LeetcodeApplicationTests.java
@@ -13,11 +13,6 @@ class LeetcodeApplicationTests {
 
     @Test
     void contextLoads() {
-//        TreeNode treeNode = TreeNode.create(new Integer[]{5, 4, 8, 11, null, 13, 4, 7, 2, null, null, null, 1});
-        ListNode[] listNodes = new ListNode[3];
-        listNodes[0]=ListNode.create(new int[]{1,4,5});
-        listNodes[1]=ListNode.create(new int[]{1,3,4});
-        listNodes[2]=ListNode.create(new int[]{2,6});
-        ListNode node = new LeetCode2().mergeKLists(listNodes);
+        List<List<Integer>> subsets = new LeetCode2().subsets(new int[]{1, 2, 3});
     }
 }
