953793aa051c142a637087ed9a0155fba1c8d889
fuchengshun <82375223@qq.com>
2020-06-02 16:57:44 +0800

    面试题42. 连续子数组的最大和

--- src/main/java/com/example/leetcode/LeetCode2.java
+++ src/main/java/com/example/leetcode/LeetCode2.java
@@ -1,6 +1,7 @@
 package com.example.leetcode;
 
 
+import java.awt.event.ItemEvent;
 import java.util.*;
 
 public class LeetCode2 {
@@ -1087,6 +1088,183 @@ public class LeetCode2 {
             cur = next;
         }
         return pre;
+    }
+
+    /**
+     * 你这个学期必须选修 numCourse 门课程，记为 0 到 numCourse-1 。
+     * <p>
+     * 在选修某些课程之前需要一些先修课程。 例如，想要学习课程 0 ，你需要先完成课程 1 ，我们用一个匹配来表示他们：[0,1]
+     * <p>
+     * 给定课程总量以及它们的先决条件，请你判断是否可能完成所有课程的学习？
+     * <p>
+     *  
+     * <p>
+     * 示例 1:
+     * <p>
+     * 输入: 2, [[1,0]]
+     * 输出: true
+     * 解释: 总共有 2 门课程。学习课程 1 之前，你需要完成课程 0。所以这是可能的。
+     * 示例 2:
+     * <p>
+     * 输入: 2, [[1,0],[0,1]]
+     * 输出: false
+     * 解释: 总共有 2 门课程。学习课程 1 之前，你需要先完成​课程 0；并且学习课程 0 之前，你还应先完成课程 1。这是不可能的。
+     *  
+     * <p>
+     * 提示：
+     * <p>
+     * 输入的先决条件是由 边缘列表 表示的图形，而不是 邻接矩阵 。详情请参见图的表示法。
+     * 你可以假定输入的先决条件中没有重复的边。
+     * 1 <= numCourses <= 10^5
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/course-schedule
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     *
+     * @param numCourses
+     * @param prerequisites
+     * @return
+     */
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> list = new ArrayList<>();
+        for (int i = 0; i < numCourses; i++) {
+            list.add(new ArrayList<>());
+        }
+        int[] count = new int[numCourses];
+        for (int[] p : prerequisites) {
+            count[p[0]]++;
+            list.get(p[1]).add(p[0]);
+        }
+        Deque<Integer> deque = new LinkedList<>();
+        for (int i = 0; i < count.length; i++) {
+            if (count[i] == 0) {
+                deque.add(i);
+            }
+        }
+        while (!deque.isEmpty()) {
+            numCourses--;
+            for (Integer i : list.get(deque.pop())) {
+                if (--count[i] == 0) {
+                    deque.add(i);
+                }
+            }
+        }
+        return numCourses == 0;
+    }
+
+    /**
+     * 现在你总共有 n 门课需要选，记为 0 到 n-1。
+     * <p>
+     * 在选修某些课程之前需要一些先修课程。 例如，想要学习课程 0 ，你需要先完成课程 1 ，我们用一个匹配来表示他们: [0,1]
+     * <p>
+     * 给定课程总量以及它们的先决条件，返回你为了学完所有课程所安排的学习顺序。
+     * <p>
+     * 可能会有多个正确的顺序，你只要返回一种就可以了。如果不可能完成所有课程，返回一个空数组。
+     * <p>
+     * 示例 1:
+     * <p>
+     * 输入: 2, [[1,0]]
+     * 输出: [0,1]
+     * 解释: 总共有 2 门课程。要学习课程 1，你需要先完成课程 0。因此，正确的课程顺序为 [0,1] 。
+     * 示例 2:
+     * <p>
+     * 输入: 4, [[1,0],[2,0],[3,1],[3,2]]
+     * 输出: [0,1,2,3] or [0,2,1,3]
+     * 解释: 总共有 4 门课程。要学习课程 3，你应该先完成课程 1 和课程 2。并且课程 1 和课程 2 都应该排在课程 0 之后。
+     *      因此，一个正确的课程顺序是 [0,1,2,3] 。另一个正确的排序是 [0,2,1,3] 。
+     * 说明:
+     * <p>
+     * 输入的先决条件是由边缘列表表示的图形，而不是邻接矩阵。详情请参见图的表示法。
+     * 你可以假定输入的先决条件中没有重复的边。
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/course-schedule-ii
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     *
+     * @param numCourses
+     * @param prerequisites
+     * @return
+     */
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        int[] count = new int[numCourses];
+        int[] ans = new int[numCourses];
+        List<List<Integer>> lists = new ArrayList<>();
+        for (int i = 0; i < numCourses; i++) {
+            lists.add(new ArrayList<>());
+        }
+        for (int[] p : prerequisites) {
+            count[p[0]]++;
+            lists.get(p[1]).add(p[0]);
+        }
+        Deque<Integer> deque = new ArrayDeque<>();
+        for (int i = 0; i < count.length; i++) {
+            if (count[i] == 0) {
+                deque.add(i);
+            }
+        }
+        int index = 0;
+        while (!deque.isEmpty()) {
+            int pop = deque.pop();
+            ans[index++] = pop;
+            numCourses--;
+            for (Integer i : lists.get(pop)) {
+                if (--count[i] == 0) {
+                    deque.add(i);
+                }
+            }
+        }
+        return numCourses == 0 ? ans : new int[]{};
+    }
+
+    /**
+     * 输入一个整型数组，数组里有正数也有负数。数组中的一个或连续多个整数组成一个子数组。求所有子数组的和的最大值。
+     * <p>
+     * 要求时间复杂度为O(n)。
+     * <p>
+     *  
+     * <p>
+     * 示例1:
+     * <p>
+     * 输入: nums = [-2,1,-3,4,-1,2,1,-5,4]
+     * 输出: 6
+     * 解释: 连续子数组 [4,-1,2,1] 的和最大，为 6。
+     *  
+     * <p>
+     * 提示：
+     * <p>
+     * 1 <= arr.length <= 10^5
+     * -100 <= arr[i] <= 100
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/lian-xu-zi-shu-zu-de-zui-da-he-lcof
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     *
+     * @param nums
+     * @return
+     */
+    public int maxSubArray(int[] nums) {
+        int[] preSum = new int[nums.length];
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            preSum[i] = sum;
+        }
+        int low = Integer.MAX_VALUE, ans = Integer.MIN_VALUE;
+        for (int i = 0; i < preSum.length; i++) {
+            low = Math.min(low, i - 1 < 0 ? 0 : preSum[i - 1]);
+            ans = Math.max(ans, preSum[i] - low);
+        }
+        return ans;
+    }
+
+    public int maxSubArray2(int[] nums) {
+        int ans = nums[0], pre = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            int temp = nums[i] + Math.max(pre, 0);
+            ans = Math.max(temp, ans);
+            pre = temp;
+        }
+        return ans;
     }
 }
 
