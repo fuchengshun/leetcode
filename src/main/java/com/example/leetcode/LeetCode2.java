6736be73db581c4b8389f39e9fdb3cec5e939853
fuchengshun <82375223@qq.com>
2020-06-10 12:02:44 +0800

    329. 矩阵中的最长递增路径

--- src/main/java/com/example/leetcode/LeetCode2.java
+++ src/main/java/com/example/leetcode/LeetCode2.java
@@ -1920,5 +1920,82 @@ public class LeetCode2 {
         }
         return ans;
     }
+
+    /**
+     * 给定一个整数矩阵，找出最长递增路径的长度。
+     * <p>
+     * 对于每个单元格，你可以往上，下，左，右四个方向移动。 你不能在对角线方向上移动或移动到边界外（即不允许环绕）。
+     * <p>
+     * 示例 1:
+     * <p>
+     * 输入: nums =
+     * [
+     * [9,9,4],
+     * [6,6,8],
+     * [2,1,1]
+     * ]
+     * 输出: 4
+     * 解释: 最长递增路径为 [1, 2, 6, 9]。
+     * 示例 2:
+     * <p>
+     * 输入: nums =
+     * [
+     * [3,4,5],
+     * [3,2,6],
+     * [2,2,1]
+     * ]
+     * 输出: 4
+     * 解释: 最长递增路径是 [3, 4, 5, 6]。注意不允许在对角线方向上移动。
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/longest-increasing-path-in-a-matrix
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     *
+     * @param matrix
+     * @return
+     */
+    public int longestIncreasingPath(int[][] matrix) {
+        if (matrix==null||matrix.length==0){
+            return 0;
+        }
+        int[][] count = new int[matrix.length][matrix[0].length];
+        int[][] direction = {{0, 1}, {0, -1}, {-1, 0}, {1, 0}};
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                for (int[] d : direction) {
+                    if (longestIncreasingPathVerify(matrix, i + d[0], j + d[1]) && matrix[i + d[0]][j + d[1]] < matrix[i][j]) {
+                        count[i][j]++;
+                    }
+                }
+            }
+        }
+        Deque<int[]> deque = new LinkedList<>();
+        for (int i = 0; i < count.length; i++) {
+            for (int j = 0; j < count[i].length; j++) {
+                if (count[i][j] == 0) {
+                    deque.add(new int[]{i, j});
+                }
+            }
+        }
+        int ans = 0;
+        while (!deque.isEmpty()) {
+            ans++;
+            for (int size = deque.size(); size > 0; size--) {
+                int[] poll = deque.poll();
+                for (int[] d : direction) {
+                    if (longestIncreasingPathVerify(matrix, poll[0] + d[0], poll[1] + d[1]) && matrix[poll[0] + d[0]][poll[1] + d[1]] > matrix[poll[0]][poll[1]]) {
+                        if (--count[poll[0] + d[0]][poll[1] + d[1]] == 0) {
+                            deque.add(new int[]{poll[0] + d[0], poll[1] + d[1]});
+                        }
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+
+    private boolean longestIncreasingPathVerify(int[][] matrix, int i, int j) {
+        return i >= 0 && j >= 0 && i < matrix.length && j < matrix[i].length;
+    }
 }
 
