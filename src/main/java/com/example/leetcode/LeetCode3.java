cbb423204a209b8e7c3eb1b5aaf10cdbd66d985e
fuchengshun <fuchengshun@gmail.com>
2020-07-05 22:26:14 +0800

    209. 长度最小的子数组

--- src/main/java/com/example/leetcode/LeetCode3.java
+++ src/main/java/com/example/leetcode/LeetCode3.java
@@ -753,4 +753,41 @@ public class LeetCode3 {
         }
         return (carry == 1 ? "1" : "") + sb.reverse().toString();
     }
+
+    /**
+     * 给定一个含有 n 个正整数的数组和一个正整数 s ，找出该数组中满足其和 ≥ s 的长度最小的子数组，
+     * 并返回其长度。如果不存在符合条件的子数组，返回 0。
+     * <p>
+     *  
+     * <p>
+     * 示例：
+     * <p>
+     * 输入：s = 7, nums = [2,3,1,2,4,3]
+     * 输出：2
+     * 解释：子数组 [4,3] 是该条件下的长度最小的子数组。
+     *  
+     * <p>
+     * 进阶：
+     * <p>
+     * 如果你已经完成了 O(n) 时间复杂度的解法, 请尝试 O(n log n) 时间复杂度的解法。
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/minimum-size-subarray-sum
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     *
+     * @param s
+     * @param nums
+     * @return
+     */
+    public int minSubArrayLen(int s, int[] nums) {
+        int left = 0, right = 0, sum = 0,ans = Integer.MAX_VALUE;
+        while (right < nums.length) {
+            sum += nums[right++];
+            while (sum >= s) {
+                ans = Math.min(ans, right - left);
+                sum -= nums[left++];
+            }
+        }
+        return Integer.MAX_VALUE == ans ? 0 : ans;
+    }
 }
