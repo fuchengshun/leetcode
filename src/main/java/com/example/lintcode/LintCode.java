751be9723662dc38c7d68f9d1db853c978613c2c
fuchengshun <fuchengshun@gmail.com>
2020-09-02 07:38:23 +0800

    486. 预测赢家

--- src/main/java/com/example/lintcode/LintCode.java
+++ src/main/java/com/example/lintcode/LintCode.java
@@ -52,4 +52,25 @@ public class LintCode {
         root.max = Math.max(root.left.max, root.right.max);
         // write your code here
     }
+
+    public boolean PredictTheWinner(int[] nums) {
+        int n = nums.length;
+        int[][][] dp = new int[n][n][2];
+        for (int i = 0; i < n; i++) {
+            dp[i][i][0] = nums[i];
+            dp[i][i][1] = 0;
+        }
+        int[] sum = new int[n+1];
+        for (int i = 0,temp=0; i < n; i++) {
+            temp+=nums[i];
+            sum[i+1]=temp;
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = i+1; j < n; j++) {
+                dp[i][j][0] = Math.max(dp[i + 1][j][1] + nums[i], dp[i][j - 1][1] + nums[j]);
+                dp[i][j][1] = sum[j+1]-sum[i]-dp[i][j][0];
+            }
+        }
+        return dp[0][n - 1][0] >= dp[0][n - 1][1];
+    }
 }
