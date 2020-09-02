2179791ca5d45f61f670505535be1b3c637d4c4e
fuchengshun <fuchengshun@gmail.com>
2020-09-02 08:16:39 +0800

    486. 预测赢家

--- src/main/java/com/example/lintcode/LintCode.java
+++ src/main/java/com/example/lintcode/LintCode.java
@@ -60,15 +60,16 @@ public class LintCode {
             dp[i][i][0] = nums[i];
             dp[i][i][1] = 0;
         }
-        int[] sum = new int[n+1];
-        for (int i = 0,temp=0; i < n; i++) {
-            temp+=nums[i];
-            sum[i+1]=temp;
-        }
-        for (int i = 0; i < n; i++) {
-            for (int j = i+1; j < n; j++) {
+//        int[] sum = new int[n + 1];
+//        for (int i = 0, temp = 0; i < n; i++) {
+//            temp += nums[i];
+//            sum[i + 1] = temp;
+//        }
+        for (int k = 1; k < n; k++) {
+            for (int i = 0, j = k; i < n && j < n; i++,j++) {
                 dp[i][j][0] = Math.max(dp[i + 1][j][1] + nums[i], dp[i][j - 1][1] + nums[j]);
-                dp[i][j][1] = sum[j+1]-sum[i]-dp[i][j][0];
+//                dp[i][j][1] = sum[j + 1] - sum[i] - dp[i][j][0];
+                dp[i][j][1] = Math.min(dp[i + 1][j][0],dp[i][j-1][0]);
             }
         }
         return dp[0][n - 1][0] >= dp[0][n - 1][1];
